78fdc5a51e4926a97e5f6de6a5a8287f27ec5a5e
Martin Hyatt <mhya0002@student.monash.edu>
2026-03-05 22:13:39 +1100

    add morse task

---
+++ p3/morse/test-your-knowledge.cpp
@@ -0,0 +1,44 @@
+#include "splashkit.h"
+
+int main()
+{
+    load_sound_effect("dit", "morse_dit.ogg");
+    load_sound_effect("dah", "morse_dah.ogg");
+
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(400);
+
+    play_sound_effect("dit");
+    delay(400);
+
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dah");
+    delay(400);
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(400);
+
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dah");
+    delay(400);
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(400);
+
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(200);
+    play_sound_effect("dit");
+    delay(200);
+}
