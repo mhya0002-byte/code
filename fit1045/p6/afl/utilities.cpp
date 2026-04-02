530434ed37a4700d381032163f43141da5c2a260
Martin Hyatt <mhya0002@student.monash.edu>
2026-04-02 14:30:56 +1100

    update utilities

--- fit1045/p6/afl/utilities.cpp
+++ fit1045/p6/afl/utilities.cpp
@@ -17,3 +17,37 @@ int read_integer(string prompt)
   }
   return to_integer(line);
 }
+
+double read_double(string prompt)
+{
+  string line = read_string(prompt);
+  while (!is_double(line))
+  {
+    write_line("Please enter a number.");
+    line = read_string(prompt);
+  }
+  return to_double(line);
+}
+
+int read_integer_range(string prompt, int bound_1, int bound_2)
+{
+    int lower_bound;
+    int upper_bound;
+    if (bound_1 <= bound_2)
+    {
+        lower_bound = bound_1;
+        upper_bound = bound_2;
+    }
+    else
+    {
+        lower_bound = bound_2;
+        upper_bound = bound_1;
+    }
+    int input = read_integer(prompt);
+    while (input < lower_bound || input > upper_bound)
+    {
+        write_line("Please enter a value between " + to_string(lower_bound) + " and " + to_string(upper_bound) + ".");
+        input = read_integer(prompt);
+    }
+    return input;
+}
--- fit1045/p7/enums/utilities.h
+++ fit1045/p7/enums/utilities.h
@@ -20,4 +20,22 @@ string read_string(string prompt);
  */
 int read_integer(string prompt);
 
+/**
+ * Read a double from the user
+ *
+ * @param prompt the message to show the user
+ * @returns the double entered
+ */
+double read_double(string prompt);
+
+/**
+ * Read an integer from the user, only accepting values between the bounds (bounds are interchangable)
+ *
+ * @param prompt the message to show the user
+ * @param bound_1 the lower bound
+ * @param bound_2 the upper bounds
+ * @returns the integer entered
+ */
+int read_integer_range(string prompt, int bound_1, int bound_2);
+
 #endif
