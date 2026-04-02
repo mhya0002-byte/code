3d29d78ae5c202c69f370c17b9f3851e9096eff7
Martin Hyatt <mhya0002@student.monash.edu>
2026-04-02 14:31:41 +1100

    remove definition of function defined in utils

--- fit1045/p7/library/library.cpp
+++ fit1045/p7/library/library.cpp
@@ -1,29 +1,6 @@
 #include "splashkit.h"
 #include "utilities.h"
 
-int read_integer_range(string prompt, int bound_1, int bound_2)
-{
-    int lower_bound;
-    int upper_bound;
-    if (bound_1 <= bound_2)
-    {
-        lower_bound = bound_1;
-        upper_bound = bound_2;
-    }
-    else
-    {
-        lower_bound = bound_2;
-        upper_bound = bound_1;
-    }
-    int input = read_integer(prompt);
-    while (input < lower_bound || input > upper_bound)
-    {
-        write_line("Please enter a value between " + to_string(lower_bound) + " and " + to_string(upper_bound) + ".");
-        input = read_integer(prompt);
-    }
-    return input;
-}
-
 struct book_data
 {
     string name;
