31b652cdb5670fbcb15663a95f895f1ed8fade9d
Martin Hyatt <mhya0002@student.monash.edu>
2026-04-15 19:39:08 +1000

    update test.py to test wl1

--- eng1013/arduino/test.py
+++ eng1013/arduino/test.py
@@ -4,12 +4,15 @@ import time
 board = pymata4.Pymata4()
 
 board.set_pin_mode_digital_output(13)
+board.set_pin_mode_digital_output(12)
 
 while True:
     try:
         board.digital_write(13, 1)
+        board.digital_write(12, 0)
         time.sleep(1)
         board.digital_write(13, 0)
+        board.digital_write(12, 1)
         time.sleep(1)
     except KeyboardInterrupt:
         board.shutdown()
