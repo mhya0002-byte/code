e56b1214d6dd822be9d22419106e09c50dee0913
Martin Hyatt <mhya0002@student.monash.edu>
2026-04-21 14:12:23 +1000

    add menu to dice

--- fit1045/p9/dice/dice.cpp
+++ fit1045/p9/dice/dice.cpp
@@ -1,5 +1,45 @@
 #include "splashkit.h"
 
+string read_string(string prompt)
+{
+    write(prompt);
+    return read_line();
+}
+
+int read_integer(string prompt)
+{
+    string line = read_string(prompt);
+    while (!is_integer(line))
+    {
+        write_line("Please enter a whole number.");
+        line = read_string(prompt);
+    }
+    return to_integer(line);
+}
+
+int read_integer_range(string prompt, int bound_1, int bound_2)
+{
+    int lower_bound;
+    int upper_bound;
+    if (bound_1 <= bound_2)
+    {
+        lower_bound = bound_1;
+        upper_bound = bound_2;
+    }
+    else
+    {
+        lower_bound = bound_2;
+        upper_bound = bound_1;
+    }
+    int input = read_integer(prompt);
+    while (input < lower_bound || input > upper_bound)
+    {
+        write_line("Please enter a value between " + to_string(lower_bound) + " and " + to_string(upper_bound) + ".");
+        input = read_integer(prompt);
+    }
+    return input;
+}
+
 class die
 {
     int sides;
@@ -28,20 +68,46 @@ public:
     }
 };
 
+void print_menu()
+{
+    write_line("1: Roll die");
+    write_line("2: Get new die");
+    write_line("3: Quit");
+}
+
+int read_menu_input()
+{
+    return read_integer_range("> ", 1, 3);
+}
+
 int main()
 {
-    die d6(6);
-    die d12(12);
+    die initial_die(6);
 
-    for (int i = 0; i < 3; i++)
-    {
-        d6.roll();
-        d6.print();
-    }
+    die current_die = initial_die;
 
-    for (int i = 0; i < 3; i++)
+    int menu_selection;
+
+    write_line("Sides: 6");
+
+    do
     {
-        d12.roll();
-        d12.print();
-    }
+        write_line();
+        print_menu();
+        menu_selection = read_menu_input();
+
+        switch (menu_selection)
+        {
+        case 1:
+            current_die.roll();
+            current_die.print();
+            break;
+
+        case 2:
+            int new_sides = read_integer("Sides: ");
+            die new_die(new_sides);
+            current_die = new_die;
+            break;
+        }
+    } while (menu_selection != 3);
 }
