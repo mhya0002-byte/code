9943db1f4f34dc1f844c5d5fcb3ec01a7cfc6c1e
Martin Hyatt <mhya0002@student.monash.edu>
2026-04-22 22:07:35 +1000

    update register

--- eng1013/arduino/12.6/register.py
+++ eng1013/arduino/12.6/register.py
@@ -3,11 +3,13 @@ import time
 board = pymata4.Pymata4()
     
 # pin setup
+srclr = 4
 ser = 5
 rclk = 6
 srclk = 7
     
 # pin mode configuration
+board.set_pin_mode_digital_output(srclr)
 board.set_pin_mode_digital_output(ser)
 board.set_pin_mode_digital_output(rclk)
 board.set_pin_mode_digital_output(srclk)
@@ -16,13 +18,19 @@ board.set_pin_mode_digital_output(srclk)
 board.digital_write(srclk, 0)
 board.digital_write(rclk, 0)
 
+
 def pulse(pin):
     board.digital_write(pin, 1)
-    time.sleep(0.1)
     board.digital_write(pin, 0)
+    
+def clear():
+    board.digital_write(srclr, 0)
+    board.digital_write(srclr, 1)
+    pulse(rclk)
 
 def write_register(state):
     for bit in state:
+
         board.digital_write(ser, bit)
 
         # push to storage register
@@ -31,24 +39,6 @@ def write_register(state):
     # copy storage register to display outputs
     pulse(rclk)
 
-pinStatus = [0, 0, 0, 0, 0, 0, 0, 0]
-
-write_register(pinStatus)
-    
-# short sleep
-time.sleep(1)
-
-pinStatus = [1, 1, 1, 1, 1, 1, 1, 1]
-
-write_register(pinStatus)
-
-time.sleep(1)
-
-pinStatus = [0, 0, 0, 0, 0, 0, 0, 0]
-
-write_register(pinStatus)
-    
-# short sleep
-time.sleep(1)
+clear()
 
 board.shutdown()
